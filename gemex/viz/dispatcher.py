faba4456f46d66d5aade2824aa87c4a0a50e5b5b
Prof. Dr. Utku Kose <60503692+utkukose@users.noreply.github.com>
2026-04-08 18:53:16 +0300

    Delete gemex directory

--- gemex/viz/dispatcher.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 Dr. Utku Kose
-# Suleyman Demirel University, Turkey | University of North Dakota, USA
-# VelTech University, India | Universidad Panamericana, Mexico
-# ORCID: 0000-0002-9652-6415 | utkukose@gmail.com | www.utkukose.com
-# Licensed under the MIT License — see LICENSE for details.
-#
-# GEMEX: Geodesic Entropic Manifold Explainability v1.2.1
-from gemex.viz.plots import VizDispatcher
-__all__ = ["VizDispatcher"]
